62e9ea444fe4d743d145c23369653dbc38ef2341
bragatheswaran-m <bragatheswaran.17ei@kct.ac.in>
2020-03-19 19:53:05 +0530

    new Commit

--- predict_user.js
+++ predict_user.js
@@ -1,9 +1,7 @@
 const video = document.getElementById('video')
 
 Promise.all([
-	faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-	faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-	faceapi.nets.faceExpressionNet.loadFromUri('/models')
+	faceapi.nets.tinyFaceDetector.loadFromUri('/models')
 ]).then(startVideo)
 
 function startVideo() {
@@ -20,16 +18,19 @@ video.addEventListener('play', () => {
 	document.getElementById("vid-container").append(canvas)
 	const displaySize = { width: video.width, height: video.height }
 	faceapi.matchDimensions(canvas, displaySize)
-	setInterval(async () => {
+	while(true){
+		setTimeout(async()=>{
 		// To Detect all faces.
 		// const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
 
 		// To detect single face.
 		const detection = await faceapi.detectSingleFace(video, new faceapi.TinyFaceDetectorOptions());
-
-		if (detection != null) {
+		canvas.getContext('2d').clearRect(0, 0, video.width, video.height);
+		if (detection != null) 
+		{
+			// Should be only here.. not anywhere else
 			const resizedDetections = faceapi.resizeResults(detection, displaySize);
-			canvas.getContext('2d').clearRect(0, 0, video.width, video.height);
+			
 			// const blob = canvas.toDataURL("image/png");
 
 			const imageObj = document.getElementById("pic");
@@ -46,51 +47,51 @@ video.addEventListener('play', () => {
 			var realData = block[1].split(",")[1];// In this case "R0lGODlhPQBEAPeoAJosM...."
 			// Convert it to a blob to upload
 			var blob = b64toBlob(realData, "png");
-			var result = await postImage(blob);
-			result = JSON.parse(result);
-			setTimeout(function () { }, 200);
-
-			// labelled face description
-			// if (result.user != "null") {
-			// 	const labeled_descriptor = new faceapi.LabeledFaceDescriptors(result.user, resizedDetections[0].descriptor)
-			// }
+			console.log(detection);
+			postImage(blob).then(res=>{
+				if (res.user != null) {
+					const box = detection._box
+					const drawBox = new faceapi.draw.DrawBox(box, { label: res.user })
+					drawBox.draw(canvas)
+				}
+				else if (res.user == null) {
+					const box = detection._box
+					const drawBox = new faceapi.draw.DrawBox(box, { label: "searching" })
+					drawBox.draw(canvas)
+				}
+			})
 			
-			
-			// face labeled descriptor drawing.
-			const box = detection._box
-			const drawBox = new faceapi.draw.DrawBox(box, { label: result.user })
-			drawBox.draw(canvas)
-
 			// face detector only.
-			faceapi.draw.drawDetections(canvas, resizedDetections);
-
+			// faceapi.draw.drawDetections(canvas, resizedDetections);
 		}
-	}, 100)
+	},150);
+	}
 })
 
-function b64toBlob(b64Data, contentType, sliceSize) {
+function b64toBlob(b64Data, contentType, sliceSize) 
+{
 	contentType = contentType || '';
 	sliceSize = sliceSize || 512;
-
 	var byteCharacters = atob(b64Data);
 	var byteArrays = [];
-
-	for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+	for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) 
+	{
 		var slice = byteCharacters.slice(offset, offset + sliceSize);
 		var byteNumbers = new Array(slice.length);
-		for (var i = 0; i < slice.length; i++) {
+		for (var i = 0; i < slice.length; i++) 
+		{
 			byteNumbers[i] = slice.charCodeAt(i);
 		}
 		var byteArray = new Uint8Array(byteNumbers);
 		byteArrays.push(byteArray);
 	}
-
 	var blob = new Blob(byteArrays, { type: contentType });
 	return blob;
 }
 
 // important function
 async function postImage(blob) {
+
 	var form_data = new FormData();
 	const myHeaders = new Headers();
 	myHeaders.append("Authorization", "Basic c3JpbmF0aDpzcm50aHNyZGhybg==");
@@ -101,8 +102,6 @@ async function postImage(blob) {
 	// myHeaders.append("Access-Control-Allow-Credentials","true");        
 	// myHeaders.append("Accept-Encoding","gzip, deflate, br");        
 	// myHeaders.append("mode","no-cors");
-
-	form_data.append("test", "test");
 	form_data.append("image_1", blob);
 	for (var i of form_data.entries()) {
 		console.log(i[0] + " " + i[1]);
@@ -113,16 +112,17 @@ async function postImage(blob) {
 		"timeout": 0,
 		"headers": {
 			"Authorization": "Basic c3JpbmF0aDpzcm50aHNyZGhybg==",
-			// "Content-Type": "multipart/form-data;"
 		},
 		"processData": false,
 		"mimeType": "multipart/form-data",
 		"contentType": false,
 		"data": form_data
 	};
-	var response = await $.ajax(requestOptions).done(function (response) { console.log(response) })
+	var result = await $.ajax(requestOptions).done(function (res) { 
+		console.log(res) 
+	})
 
-	response = JSON.parse(response)
+	response = JSON.parse(result)
 	document.getElementById("user").textContent = response.message
 	return response;
 
