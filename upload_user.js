dbd652d0b16136a5b22b02f9881920df1fa9d799
Aswath S <aswath7862001@gmail.com>
2020-04-06 11:16:16 +0530

    Update upload_user.js

--- upload_user.js
+++ upload_user.js
@@ -129,7 +129,7 @@ video.addEventListener('play', () => {
         src = canvas.toDataURL("image/jpeg");
 
         imageObj.src = src;
-        imageObj.style = "display:inline;float:right;width:500px;height:35vh;"
+        imageObj.style = "display:inline;float:right;width:400px;height:400px;"
         // console.log(src);  
         pic_array.push(src);
 
